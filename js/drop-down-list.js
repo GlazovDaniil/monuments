d07163583f91c01da9ce078273d019f0e2c65cc3
DaniilGlazov <d.glazow2016@yandex.ru>
2023-08-28 21:53:40 +0300

    Version 1.0

--- js/drop-down-list.js
+++ js/drop-down-list.js
@@ -1,20 +1,17 @@
-const selectSingle = document.querySelector('.__select');
-const selectSingle_title = selectSingle.querySelector('.__select__title');
-const selectSingle_labels = selectSingle.querySelectorAll('.__select__label');
-
-// Переключать меню
-selectSingle_title.addEventListener('click', () => {
-if ('active' === selectSingle.getAttribute('data-state')) {
+  // Переключать меню 
+  function  switch_menu (selectSingle){
+    if ('active' === selectSingle.getAttribute('data-state')) {
     selectSingle.setAttribute('data-state', '');
-} else {
+    } else {
     selectSingle.setAttribute('data-state', 'active');
+  }
 }
-});
-
-// Закрыть при нажатии на опцию
-for (let i = 0; i < selectSingle_labels.length; i++) {
-selectSingle_labels[i].addEventListener('click', (evt) => {
-    selectSingle_title.textContent = evt.target.textContent;
-    selectSingle.setAttribute('data-state', '');
-});
+function clouse_menu (selectSingle ,selectSingle_labels, selectSingle_title){
+  // Закрыть при нажатии на опцию
+  for (let i = 0; i < selectSingle_labels.length; i++) {
+    selectSingle_labels[i].addEventListener('click', (evt) => {
+      selectSingle_title.textContent = evt.target.textContent;
+      selectSingle.setAttribute('data-state', '');
+    });
+  }
 }
